f37378b3042a76ed15d70414e74464934444284c
danylin <danylorva_16@hotmail.com>
2021-09-14 12:41:50 -0500

    cambio t5

--- include/creacion_evento.php
+++ include/creacion_evento.php
@@ -2,9 +2,9 @@
     <div class="table__title">Eventos Actuales</div>
     <?php
     include("bd_usuario.php");
-    $sql="SELECT*FROM eventos_db";
+    $sql="SELECT*FROM evento_acc_db;";
     $resultado=mysqli_query($conexion,$sql);
-    while($row=mysqli_fetch_array()){
+    while($row=mysqli_fetch_array($resultado)){
         echo "<div class='table__item'>". $row['fecha']."</div>";
         echo "<div class='table__item'>". $row['fecha']."</div>";
         echo "<div class='table__item'>". $row['fecha']."</div>";
